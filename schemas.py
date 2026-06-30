efcb442451201d0f6d75bc5681d5f3bdc17c98d5
iris <irisorris02@gmail.com>
2026-06-30 11:47:08 +0530

    test commit

--- schemas.py
+++ schemas.py
@@ -18,6 +18,4 @@ class UserUpdate(BaseModel):
     email: Optional[EmailStr] = None
     password: Optional[str] = None
 
-class Customer(BaseModel):
-    email: Optional[EmailStr] = None
-    password: Optional[str] = None
+
